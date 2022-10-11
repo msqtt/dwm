d33703900e50c2004a30df01f2f5797821bbdeea
mosqu1t0 <mosquito@email.cn>
2022-10-11 13:24:01 +0800

    viewontag

--- config.def.h
+++ config.def.h
@@ -10,6 +10,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
+static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=10",
                                         "WenQuanYi Micro Hei Mono:size=10:style=Bold:antialias=true:autohint=true",
                                         "Symbols Nerd Font Mono:style=1000-em:size=9"
